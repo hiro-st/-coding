b42fddee2a7e811bd1fc9bbe6fcbc0c3f491a5d3
hiro-st <st.hiroko.1104@gmail.com>
2024-02-15 08:32:16 +0900

    cafesiteの課題提出

---
+++ cafesite/js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+    //   /*=================================================
+    // トップに戻る
+    // ===================================================*/
+    let pagetop = $(".to-top");
+    pagetop.hide();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+
+    pagetop.click(function () {
+        $("body,html").animate({ scrollTop: 0 }, 500, "swing");
+        return false;
+    });
+
+    //   /*=================================================
+    //スムーススクロール
+    // ===================================================*/
+
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, 1000, "swing");
+        return false;
+    });
+
+
+});
